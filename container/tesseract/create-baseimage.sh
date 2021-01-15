c344fe5de1aa182187106133248880df388bdb5f
Uwe Hartwig <uwe.hartwig@bitsrc.info>
2021-01-15 14:24:50 +0100

    [app][rfct] re-design close #8

--- container/tesseract/create-baseimage.sh
+++ container/tesseract/create-baseimage.sh
@@ -4,7 +4,7 @@ set -eu
 
 IMAGE_NAME=$1
 TESSERACT_RELEASE=$2
-DOCKER_CONF=Dockerfile-tesseract
+DOCKER_CONF=Dockerfile
 TESSERACT_REPOSITORY=https://github.com/ulb-sachsen-anhalt/tesseract.git
 
 docker build --build-arg TESSERACT_RELEASE="${TESSERACT_RELEASE}" --build-arg TESSERACT_REPOSITORY=${TESSERACT_REPOSITORY} --tag "${IMAGE_NAME}:${TESSERACT_RELEASE}" -f ${DOCKER_CONF} .

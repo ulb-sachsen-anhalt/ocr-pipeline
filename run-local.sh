980ea9fa8df812e445843a73c13fa3abe24b6389
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-01-22 16:19:27 +0100

    [app][feat] tesseract params as json dict

--- run-local.sh
+++ run-local.sh
@@ -18,16 +18,18 @@ source ./${ENV_NAME}/bin/activate
 
 # $1 => scandata_path
 # $2 => work_dir
-# $3 => image dpi
-# $4 => Tesseract configuration
+# deprecated  $3 => image dpi
+# $3 => parameters for tesseract as json e.g.: 
+#    '{"--dpi":100, "--psm":1, "--oem":1, "-l": "frk+deu"}' 
+# deprecated $4 => Tesseract configuration
 
 [[ $1 == */ ]] && PATTERN=${1%/*} || PATTERN=$1
 if [ -n "$2" ]; then
     if [ -n "$3" ]; then
         if [ -n "$4" ]; then
-            python ocr_pipeline.py -s "$PATTERN" -w $2 -d $3 -m $4
+            python ocr_pipeline.py -s "$PATTERN" -w $2 -x "$3" 
         else
-            python ocr_pipeline.py -s "$PATTERN" -w $2 -d $3
+            python ocr_pipeline.py -s "$PATTERN" -w $2 -x "$3"
         fi
     else
         python ocr_pipeline.py -s "$PATTERN" -w $2 

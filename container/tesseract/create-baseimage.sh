a2bf4930a9f8fa33b2e59a870ce33ce24ba29120
Uwe Hartwig <uwe.hartwig@bitsrc.info>
2021-02-02 22:01:21 +0100

    [app][feat] clear basis image fix#11

--- container/tesseract/create-baseimage.sh
+++ container/tesseract/create-baseimage.sh
@@ -2,9 +2,15 @@
 
 set -eu
 
-IMAGE_NAME=${1/:*/}
-TESSERACT_RELEASE=${1/*:/}
-DOCKER_CONF=Dockerfile
-TESSERACT_REPOSITORY=https://github.com/ulb-sachsen-anhalt/tesseract.git
+IMAGE=${1}
+TESSERACT_REF=${2}
+DOCKER_CONF=container/tesseract/Dockerfile
+TESSERACT_REPOSITORY=https://github.com/tesseract-ocr/tesseract.git
 
-docker build --build-arg TESSERACT_RELEASE="${TESSERACT_RELEASE}" --build-arg TESSERACT_REPOSITORY=${TESSERACT_REPOSITORY} --tag "${IMAGE_NAME}:${TESSERACT_RELEASE}" -f ${DOCKER_CONF} .
+cd ../..
+
+docker build --no-cache \
+    --build-arg TESSERACT_REF="${TESSERACT_REF}" \
+    --build-arg TESSERACT_REPOSITORY=${TESSERACT_REPOSITORY} \
+    --tag "${IMAGE}" \
+    -f ${DOCKER_CONF} .
